742ecc558c158a3eb450ae2318e77b85c4bdb019
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-13 23:00:05 -0700

    Users can logout

--- app/src/main/java/com/elmeradrianv/instagramcloneapp/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.elmeradrianv.instagramcloneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    ParseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.btnLogout:
+                ParseUser.logOut();
+                currentUser = ParseUser.getCurrentUser(); // this will now be null
+                //Log.d(TAG, "onOptionsItemSelected: "+currentUser);
+                Intent i = new Intent(this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+                startActivity(i);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
