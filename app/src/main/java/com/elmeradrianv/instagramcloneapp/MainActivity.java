15ac2c9d19ff65662c03f9e65f470553f96e82d1
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-14 14:03:18 -0700

    PostAdapter and show 20 posts in the feed

--- app/src/main/java/com/elmeradrianv/instagramcloneapp/MainActivity.java
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/MainActivity.java
@@ -1,30 +1,50 @@
 package com.elmeradrianv.instagramcloneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.elmeradrianv.instagramcloneapp.adapters.PostAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class   MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     ParseUser currentUser;
-
+    protected PostAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
+        RecyclerView rvPosts;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getMenuInflater();
         currentUser=ParseUser.getCurrentUser();
+        rvPosts = findViewById(R.id.rvPosts);
 
+        // initialize the array that will hold posts and create a PostsAdapter
+        adapter = new PostAdapter(this);
+
+        // set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        // query posts from Parstagram
+        queryPosts();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -45,6 +65,33 @@ public class   MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void queryPosts() {
+        // specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include data referred by user key
+        query.include(Post.KEY_USER);
+        query.include(Post.KEY_IMAGE);
+        // limit query to latest 20 items
+        query.setLimit(20);
+        // order posts by creation date (newest first)
+        query.addDescendingOrder("createdAt");
+        // start an asynchronous call for posts
+        query.findInBackground((posts, e) -> {
+            // check for errors
+            if (e != null) {
+                Log.e(TAG, "Issue with getting posts", e);
+                return;
+            }
+
+            // for debugging purposes let's print every post description to logcat
+            for (Post post : posts) {
+                Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+            }
+
+            // save received posts to list and notify adapter of new data
+            adapter.addAll(posts);
+        });
     }
 
 }
--- app/src/main/java/com/elmeradrianv/instagramcloneapp/adapters/PostAdapter.java
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/adapters/PostAdapter.java
@@ -1,24 +1,132 @@
 package com.elmeradrianv.instagramcloneapp.adapters;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PostAdapter extends RecyclerView.Adapter {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.elmeradrianv.instagramcloneapp.Post;
+import com.elmeradrianv.instagramcloneapp.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+    List<Post> posts;
+    Context context;
+
+    public PostAdapter() {
+        super();
+    }
+
+    public PostAdapter(Context context) {
+        this.posts = new ArrayList<>();
+        this.context=context;
+    }
+
+    public void clear(){
+        posts.clear();
+        notifyDataSetChanged();
+    }
+    public void addAll(List<Post> list){
+        posts.addAll(list);
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.post_view,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        //Get the data at position
+        Post post = posts.get(position);
+        //Bind the tweet with view holder
+        holder.bind(post);
     }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return posts.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvDescription;
+        TextView tvUsername;
+        TextView tvUsernameDescription;
+        TextView tvTimeAgo;
+        ImageView ivPost;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            tvDescription=itemView.findViewById(R.id.tvDescription);
+            tvUsername=itemView.findViewById(R.id.tvUsername);
+            tvUsernameDescription=itemView.findViewById(R.id.tvUsernameDescription);
+            tvTimeAgo = itemView.findViewById(R.id.tvTimeAgo);
+            ivPost=itemView.findViewById(R.id.ivPost);
+        }
+
+        public void bind(Post post) {
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            tvUsernameDescription.setText(post.getUser().getUsername());
+            tvTimeAgo.setText(calculateTimeAgo(post.getCreatedAt()));
+            Glide.with(context).load(post.getImage().getUrl())
+                    .apply(new RequestOptions()
+                            .centerCrop() // scale image to fill the entire ImageView
+
+                    )
+                    .into(ivPost);
+        }
+    }
+    public static String calculateTimeAgo(Date createdAt) {
+
+        int SECOND_MILLIS = 1000;
+        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        try {
+            createdAt.getTime();
+            long time = createdAt.getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (Exception e) {
+            Log.i("Error:", "getRelativeTimeAgo failed", e);
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
