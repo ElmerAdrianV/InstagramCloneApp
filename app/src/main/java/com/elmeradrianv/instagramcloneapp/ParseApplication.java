5cdb37dd5653bbbb04565cd4ec00bb9f7a63621f
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-13 22:50:48 -0700

    Create an Application class, and initialize the Parse SDK with your Parse application ID, client key, and server.

---
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.elmeradrianv.instagramcloneapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+   private static final String applicationId= BuildConfig.APP_ID;
+   private static final String clientKey = BuildConfig.CLIENT_KEY;
+   private static final String server = "https://parseapi.back4app.com/";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(applicationId)
+                .clientKey(clientKey)
+                .server(server).build());
+
+    }
+}
