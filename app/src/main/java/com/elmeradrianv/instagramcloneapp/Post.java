45de2fef66b7038ebd99968ecc02c5f747121ffd
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-14 13:59:35 -0700

    Prepare Post for the recycle view

--- app/src/main/java/com/elmeradrianv/instagramcloneapp/Post.java
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/Post.java
@@ -5,11 +5,16 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.io.File;
+import java.util.Date;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION="description";
     public static final String KEY_IMAGE="image";
     public static final String KEY_USER="user";
+    public static final String KEY_CREATED_AT="createdAt";
+
 
     public String getDescription(){
         return getString(KEY_DESCRIPTION);
@@ -33,5 +38,8 @@ public class Post extends ParseObject {
         put(KEY_USER, parseUser);
     }
 
+    public Date getCreateAt(){
+        return getDate(KEY_CREATED_AT);
+    }
 
 }
