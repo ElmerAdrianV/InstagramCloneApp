30c38b72f3549db59326714b78438effbbc35a3a
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-14 15:58:53 -0700

    Display the app icon on the launch screen

---
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/LaunchActivity.java
@@ -0,0 +1,23 @@
+package com.elmeradrianv.instagramcloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        },2000);
+    }
+}
