c5d81454e7ee9487caa52cad922251f85c74807f
ElmerAdrianV <71801888+ElmerAdrianV@users.noreply.github.com>
2022-06-15 11:24:47 -0700

    Create the PostDetailView with input and display of comments

---
+++ app/src/main/java/com/elmeradrianv/instagramcloneapp/PostDetailView.java
@@ -0,0 +1,13 @@
+package com.elmeradrianv.instagramcloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail_view);
+    }
+}
